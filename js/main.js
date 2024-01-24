eaec3572e2e08b5f504af89eba962501975cb416
EmranRifat <imran15-1919@diu.edu.bd>
2024-01-24 15:24:54 +0600

    updated resume

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   //========= typing text animation script =========
   var typed = new Typed(".typing", {
-    strings: ["MERN Stack Developer", "FrontEnd Developer", "Web Designer"],
+    strings: ["MERN Stack Developer", "FrontEnd Developer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
